b3d1302053de62d9c9fe4db2d735203ac14e48ec
Adam Lesperance <lespea@gmail.com>
2009-12-06 16:38:39 -0600

    Added the check method to the module

--- lib/Project/Euler/Lib/MultipleCheck.pm
+++ lib/Project/Euler/Lib/MultipleCheck.pm
@@ -2,7 +2,7 @@ package Project::Euler::Lib::MultipleCheck;
 
 use Modern::Perl;
 use Moose;
-use Moose::Util::TypeConstraints;
+use MooseX::Method::Signatures;
 
 use Carp;
 use Readonly;
@@ -63,115 +63,35 @@ has 'check_all' => (
 
 =head1 FUNCTIONS
 
-    _check_input
-    _solve_problem
+=head2 check
+
+Function that returns a Boolean if the given number passes the checks
+
+    my $check = Project::Euler::Lib::MultipleCheck->new(
+        multi_nums => [3, 5];
+        check_all  => 0,
+    );
+
+    my $true  = $multi_check->check(9);
+    my $false = $multi_check->check(11);
+
+    $multi_check->check_all(1);
+    my $true  = $multi_check->check(15);
+    my $false = $multi_check->check(10);
+
+
+    $dies = $multi_check->multi_nums([0, 1]);  # Multi_nums must all be positive
+    $dies = $multi_check->multi_nums((2, 9));  # Multi nums must be an array ref
+
+    $dies = $multi_check->check('test');  # Can't check a num
 
 =cut
 
-requires '_check_input';
-requires '_solve_problem';
 
-
-
-#  Internal function
-sub _build_problem_link {
-    my ($self) = @_;
-    return $BASE_URL . $self->problem_number;
-}
-
-
-
-=head1 PROVIDED FUNCTIONS
-
-=head2 solve
-
-This is the function that should be called to solve the problem.  Depending on
-the object attributes that are set, it uses either the default or provided
-inputs if they are required and returns the answer as a string
-
-    my $problem_1  = Project::Euler::Problem::P001->new();
-    my $def_answer = $problem_1->solve;
-
-    $problem_1->custom_input  => (42);
-    $problem_1->custom_answer => (42);
-    $problem_1->use_defaults  => (21);
-    my $custom_answer = $problem_1->solve;
-
-=cut
-
-sub solve {
-    my ($self) = @_;
-    my $answer;
-
-    #  There may be parameters to pass so get determine what they are and pass
-    #  them
-    if ( $self->has_input ) {
-        #  Use the module_provided defaults (ie pass nothing)
-        if ( $self->use_defaults ) {
-            $answer = $self->_solve_problem;
-        }
-        #  Pass the user input to the subroutine
-        elsif (defined $self->custom_input) {
-            $answer = $self->_solve_problem( $self->custom_input );
-        }
-        #  The user tried to use a cutsom input string to
-        #  solve the problem but hasn't defined it yet!
-        else {
-            croak q{You tried to use custom inputs to solve the problem but it has not been set yet}
-        }
-    }
-
-    #  There are no paramaters to pass!
-    else {
-        $answer = $self->_solve_problem;
-    }
-
-
-    #  Determine if the given answer was correct :: use 0 rather than undef
-    my $wanted  =  $self->use_defaults  ?  $self->default_answer  :  $self->custom_answer;
-
-    $answer //= q{};  $wanted //= q{};
-
-    my $status  =  $answer eq $wanted;
-
-    #  Save the answer, wanted, and status
-    $self->_set_solved_answer($answer);
-    $self->_set_solved_wanted($wanted);
-    $self->_set_solved_status($status);
-
-    #  Return either the status, answer, and wanted or, if the user just
-    #  expects a scalar, the found answer
-    return wantarray  ?  ($status, $answer, $wanted)  :  $answer;
-}
-
-
-
-
-=head2 status
-
-This function simply returns a nice, readable status message that tells you the
-outcome of the last run of the module.  This is way the array won't have to be
-parsed every time to determine the various states that are saved.
-
-    my $problem_1  = Project::Euler::Problem::P001->new();
-    $problem_1->solve;
-    my $message = $problem_1->last_run_message;
-
-=cut
-
-sub status {
-    my ($self) = @_;
-    my ($answer, $wanted, $status) =
-        @{$self}{qw/ solved_answer  solved_wanted  solved_status /};
-
-    if (!defined $status) {
-        return q{It appears that the problem has yet to be solved once.};
-    }
-    else {
-        return printf(q{The last run was%s succesfull!  The answer expected was '%s' %s the answer returned was '%s'},
-            $status ? q{} : 'not', $wanted, $status ? 'and' : 'but', $answer
-        );
-    }
+method check (Lib::PosInt $num) {
+    my $multi_nums = $self->multi_nums;
+    return  $self->check_all  ?  all {($num % $_) == 0} @$multi_nums
+                              :  any {($num % $_) == 0} @$multi_nums
 }
 
 
