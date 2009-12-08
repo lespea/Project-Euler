534b10e4707ecd48ff4ba0a5450340b0bbd20b33
Adam Lesperance <lespea@gmail.com>
2009-12-08 00:25:09 -0600

    Updated POD documentiont + migrated subtypes to P:E:L:Types

--- lib/Project/Euler/Problem/Base.pm
+++ lib/Project/Euler/Problem/Base.pm
@@ -4,7 +4,7 @@ use Modern::Perl;
 use Moose::Role;
 use Moose::Util::TypeConstraints;
 
-use Project::Euler::Lib::Types  qw/ PosInt  MyDateTime /;
+use Project::Euler::Lib::Types  qw/ ProblemLink  ProblemName  PosInt  MyDateTime /;
 
 use Carp;
 use DateTime;
@@ -20,53 +20,23 @@ Project::Euler::Problem::Base - Abstract class that the problems will extend fro
 
 =head1 VERSION
 
-Version v0.2.0
+Version v0.2.1
 
 =cut
 
-use version 0.77; our $VERSION = qv("v0.2.0");
+use version 0.77; our $VERSION = qv("v0.2.1");
 
 
 =head1 SYNOPSIS
 
-To ensure that each problem class performs a minimum set of functions, this
-class will define the basic subroutines and variables that every object must
-implement.  This will make wrapping a gui around them much simpler in the
-future while also eliminating a lot of confustion that could arise from having
-different method names/conventions
-
+    use Moose;
     with Project::Euler::Problem::Base;
 
+=head1 DESCRIPTION
 
-=head1 SUBTYPES
-
-Create the subtypes that we will use to validate the arguments defined by the
-extending classes
-
-    Base::link      = $link =~ m_ \A \Qhttp://projecteuler.net/index.php?section=problems&id=\E \d+ \z _xms
-    Base::prob_name = str  &&  10 < len < 80
-
-We also tell Moose how to coerce a given string into a DateTime object
-
-=cut
-
-subtype 'Base::link'
-    => as 'Str'
-    => message { "$_ is not a a valid link" }
-    => where {
-        $_ =~ m{ \A \Q$BASE_URL\E \d+ \z }xms;
-    }
-;
-
-subtype 'Base::prob_name'
-    => as 'Str'
-    => message { "$_ must be a a string between 10 and 80 characters long" }
-    => where {
-        length $_ > 10  and  length $_ < 80;
-    }
-;
-
-
+To ensure that each problem class performs a minimum set of functions, this
+class will define the basic subroutines and variables that every object must
+implement.
 
 
 =head1 VARIABLES
@@ -76,23 +46,23 @@ extending module will be changing these values, we will force them to create
 functions which will set the attributes.  We also declare the init_arg as undef
 so nobody creating an instance of the problem can over-write the values.
 
-    problem_number ( PosInt     ) # Problem number on projecteuler.net
-    problem_name   ( prob_name  ) # Short name given by the module author
-    problem_date   ( MyDateTime ) # Date posted on projecteuler.net
-    problem_desc   ( str        ) # Description posted on projecteuler.net
-    problem_link   ( URL        ) # URL to the problem's homepage
+    problem_number ( PosInt      )  # Problem number on projecteuler.net
+    problem_name   ( ProblemName )  # Short name given by the module author
+    problem_date   ( MyDateTime  )  # Date posted on projecteuler.net
+    problem_desc   ( str         )  # Description posted on projecteuler.net
+    problem_link   ( ProblemLink )  # URL to the problem's homepage
 
-    default_input  ( str        ) # Default input posted on projecteuler.net
-    default_answer ( str        ) # Default answer to the default input
+    default_input  ( str         )  # Default input posted on projecteuler.net
+    default_answer ( str         )  # Default answer to the default input
 
-    has_input      ( boolean    ) # Some problems might not have so this lets us disable it
-    use_defaults   ( boolean    ) # Use the default inputs
+    has_input      ( boolean     )  # Some problems may not have use for input
+    use_defaults   ( boolean     )  # Use the default inputs
 
-    custom_input   ( str        ) # User provided input to the problem
-    custom_answer  ( str        ) # User provided answer to the problem
+    custom_input   ( str         )  # User provided input to the problem
+    custom_answer  ( str         )  # User provided answer to the problem
 
-    solve_status   ( boolean    ) # True means it was valid
-    solve_answer   ( str        ) # Last answer provided
+    solve_status   ( boolean     )  # True means it was valid
+    solve_answer   ( str         )  # Last answer provided
 
 =cut
 
@@ -107,7 +77,7 @@ requires '_build_problem_number';
 
 has 'problem_name' => (
     is         => 'ro',
-    isa        => 'Base::prob_name',
+    isa        => ProblemName,
     required   => 1,
     lazy_build => 1,
     init_arg   => undef,
@@ -144,11 +114,15 @@ has 'problem_link_base' => (
 
 has 'problem_link' => (
     is         => 'ro',
-    isa        => 'Base::link',
+    isa        => ProblemLink,
     required   => 1,
     lazy_build => 1,
     init_arg   => undef,
 );
+sub _build_problem_link {
+    my ($self) = @_;
+    return $BASE_URL . $self->problem_number;
+}
 
 has 'default_input' => (
     is         => 'ro',
@@ -236,10 +210,17 @@ has 'solved_wanted'  => (
 
 
 
-=head1 FUNCTIONS
+=head1 ABSTRACT FUNCTIONS
 
-    _check_input
-    _solve_problem
+These two functions must also be overridden by the extending class
+
+=head2 _check_input
+
+Ensure the input provided by the user is compliant
+
+=head2 _solve_problem
+
+This the main function which will return the status/answer for a problem
 
 =cut
 
@@ -247,30 +228,26 @@ requires '_check_input';
 requires '_solve_problem';
 
 
-
-#  Internal function
-sub _build_problem_link {
-    my ($self) = @_;
-    return $BASE_URL . $self->problem_number;
-}
-
-
-
 =head1 PROVIDED FUNCTIONS
 
 =head2 solve
 
-This is the function that should be called to solve the problem.  Depending on
-the object attributes that are set, it uses either the default or provided
-inputs if they are required and returns the answer as a string
+This function will point to the internal function that actually solves the
+problem..  Depending on the object attributes that are set, it uses either the
+default or provided inputs (if they are required) and returns the answer as a
+string in scalar context, or an array containing the status, calculated answer,
+and expected answer.
 
     my $problem_1  = Project::Euler::Problem::P001->new();
     my $def_answer = $problem_1->solve;
 
     $problem_1->custom_input  => (42);
     $problem_1->custom_answer => (42);
-    $problem_1->use_defaults  => (21);
+    $problem_1->use_defaults  => (1);
+
     my $custom_answer = $problem_1->solve;
+
+    my ($status, $answer, $expected) = $problem_1->solve;
 
 =cut
 
@@ -278,23 +255,24 @@ sub solve {
     my ($self, $cust_input) = @_;
     my $answer;
 
+    #  If no input was given as an arg, try to get it from the current object.
+    #  This may still return an undef but that's alright
     $cust_input //= $self->custom_input;
 
-    #  There may be parameters to pass so get determine what they are and pass
-    #  them
+    #  If there problem takes input, determine the appropriate course of action
     if ( $self->has_input ) {
-        #  Use the module_provided defaults (ie pass nothing)
+        #  The user wants to use the defaults so don't pass anything
         if ( $self->use_defaults ) {
             $answer = $self->_solve_problem;
         }
-        #  Pass the user input to the subroutine
+        #  Pass the user input to the subroutine (if it's defined!)
         elsif (defined $cust_input) {
             $answer = $self->_solve_problem( $cust_input );
         }
         #  The user tried to use a cutsom input string to
         #  solve the problem but hasn't defined it yet!
         else {
-            croak q{You tried to use custom inputs to solve the problem but it has not been set yet}
+            croak q{You tried to use custom inputs to solve the problem, but it has not been set yet}
         }
     }
 
@@ -304,11 +282,15 @@ sub solve {
     }
 
 
-    #  Determine if the given answer was correct :: use 0 rather than undef
-    my $wanted  =  $self->use_defaults  ?  $self->default_answer  :  $self->custom_answer;
+    # Determine what the expected answer should be, depending on whether the
+    # defaults were used or not.
+    my $wanted = $self->use_defaults  ?  $self->default_answer  :  $self->custom_answer;
 
+    #  Determine if the given answer was correct.
+    #  Use a blank string rather than undef for the given and expected answer
     $answer //= q{};  $wanted //= q{};
 
+    #  See if the answer was correct
     my $status  =  $answer eq $wanted;
 
     #  Save the answer, wanted, and status
@@ -318,17 +300,15 @@ sub solve {
 
     #  Return either the status, answer, and wanted or, if the user just
     #  expects a scalar, the found answer
-    return wantarray  ?  ($status, $answer, $wanted)  :  $answer;
+    return  wantarray  ?  ($status, $answer, $wanted)  :  $answer;
 }
-
 
 
 
 =head2 status
 
 This function simply returns a nice, readable status message that tells you the
-outcome of the last run of the module.  This is way the array won't have to be
-parsed every time to determine the various states that are saved.
+outcome of the last run of the module.
 
     my $problem_1  = Project::Euler::Problem::P001->new();
     $problem_1->solve;
@@ -338,19 +318,23 @@ parsed every time to determine the various states that are saved.
 
 sub status {
     my ($self) = @_;
+
+    #  Extract the status and solved and expected answer
     my ($answer, $wanted, $status) =
         @{$self}{qw/ solved_answer  solved_wanted  solved_status /};
 
+    #  If the status isn't even defined then the problem wasn't even run
     if (!defined $status) {
         return q{It appears that the problem has yet to be solved once.};
     }
+
+    #  Otherwise print a message if it failed or not
     else {
-        return printf(q{The last run was%s succesfull!  The answer expected was '%s' %s the answer returned was '%s'},
-            $status ? q{} : 'not', $wanted, $status ? 'and' : 'but', $answer
+        return sprintf(q{The last run was%s succesfull!  The answer expected was '%s' %s the answer returned was '%s'},
+            $status ? q{} : ' not', $wanted, $status ? 'and' : 'but', $answer
         );
     }
 }
-
 
 
 
