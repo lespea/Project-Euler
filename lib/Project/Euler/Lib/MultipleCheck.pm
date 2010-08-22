09f3a3e59bf3dc828618a5cda2717799eb27bce4
Adam Lesperance <lespea@gmail.com>
2010-08-22 11:59:58 -0500

    Switch over to Dist::Zilla & clean up perldoc

--- lib/Project/Euler/Lib/MultipleCheck.pm
+++ lib/Project/Euler/Lib/MultipleCheck.pm
@@ -1,31 +1,21 @@
+use strict;
+use warnings;
 package Project::Euler::Lib::MultipleCheck;
 
 use Modern::Perl;
+use namespace::autoclean;
+
 use Moose;
 use MooseX::Method::Signatures;
 
 use List::MoreUtils qw/ any  all /;
-
 use Project::Euler::Lib::Types qw/ PosInt  PosIntArray /;
 
 
-=head1 NAME
-
-Project::Euler::Lib::MultipleCheck - Determine if an integer is divisible by an array of numbers
-
-=head1 VERSION
-
-Version v0.1.1
-
-=cut
-
-use version 0.77; our $VERSION = qv("v0.1.1");
+#ABSTACT: Determine if an integer is divisible by an array of numbers
 
 
 =head1 SYNOPSIS
-
-Module that is used to determine if a number is a multiple of any (or
-optionally all) numbers in an array
 
     use Project::Euler::Lib::MultipleCheck;
     my $multi_check = Project::Euler::Lib::MultipleCheck->new(
@@ -44,18 +34,20 @@ array C<< [2] >>.  A boolean is also used to determining if the number should
 be divisible by all of the integers in the array or if any will suffice.
 
 The array of integers is always sorted to maximize efficiency (lower numbers
-have a better chance of matching over higher ones)
+have a better chance of matching over higher ones).
 
 
-=head1 VARIABLES
 
-The numbers to test against
+=attr multi_nums
 
-    multi_nums ( ArrayRef[PosInts] )
+The numbers to test against.
 
-The check number must be divisible by all numbers in the array
+=head3 Definition
 
-    check_all  ( Bool )
+    is          => 'rw',
+    isa         => PosIntArray,
+    lazy_build  => 1,
+    required    => 1,
 
 =cut
 
@@ -66,6 +58,20 @@ has 'multi_nums' => (
     required    => 1,
 );
 
+
+=attr check_all
+
+The check number must be divisible by all numbers in the array.
+
+=head3 Definition
+
+    is          => 'rw',
+    isa         => 'Bool',
+    required    => 1,
+    default     => 0,
+
+=cut
+
 has 'check_all' => (
     is          => 'rw',
     isa         => 'Bool',
@@ -75,29 +81,35 @@ has 'check_all' => (
 
 
 
-=head1 FUNCTIONS
+=method check
 
-=head2 check
+Function that returns a Boolean if the given number passes the checks.
 
-Function that returns a Boolean if the given number passes the checks
+=head3 Paramaters
+
+=for :list
+= num
+The number that will be checked for divisibility of the multi_nums attribute.
+
+=head3 Example
 
     my $check = Project::Euler::Lib::MultipleCheck->new(
         multi_nums => [3, 5],
         check_all  => 0,
     );
 
-    OK      $multi_check->check(9);
+    OK      $multi_check->check( 9);
     NOT OK  $multi_check->check(11);
 
 
     $multi_check->check_all(1);
 
     OK      $multi_check->check(15);
-    NOT OK  $multi_check->check(10);
+    NOT OK  $multi_check->check( 9);
 
 
     DIES    $multi_check->multi_nums([0, 1]);  # Multi_nums must all be positive
-    DIES    $multi_check->multi_nums(2, 9);    # Multi nums must be an array ref
+    DIES    $multi_check->multi_nums( 2, 9 );  # Multi nums must be an array ref
 
     DIES    $multi_check->check('two');        # Can't check a string!
 
@@ -111,40 +123,4 @@ method check (PosInt $num) {
 }
 
 
-
-
-=head1 AUTHOR
-
-Adam Lesperance, C<< <lespea at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-project-euler at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Project-Euler>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Project::Euler::Lib::MultipleCheck
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Adam Lesperance.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
-
-no Moose;
 1; # End of Project::Euler::Lib::MultipleCheck
--- lib/Project/Euler/Lib/Types.pm
+++ lib/Project/Euler/Lib/Types.pm
@@ -1,6 +1,11 @@
+use strict;
+use warnings;
 package Project::Euler::Lib::Types;
 
 use Modern::Perl;
+use namespace::autoclean;
+
+#ABSTRACT: Type definitions for L<< Project::Euler >>
 
 
 #  Declare our types
@@ -16,22 +21,12 @@ use MooseX::Types
 #  Import builtin types
 use MooseX::Types::Moose qw/ Str  Int  ArrayRef /;
 
-=head1 NAME
-
-Project::Euler::Lib::Types - Type definitions for L<< Project::Euler >>
-
-=head1 VERSION
-
-Version v0.1.2
-
-=cut
-
-use version 0.77; our $VERSION = qv("v0.1.2");
 
 
 =head1 SYNOPSIS
 
-    use Project::Euler::Lib::Types  qw/ (types to import) /;
+    use Project::Euler::Lib::Types  qw/ ProblemLink  PosInt /;
+
 
 =head1 DESCRIPTION
 
@@ -43,14 +38,16 @@ from the global declarations.
 =head1 SUBTYPES
 
 Create the subtypes that we will use to validate the arguments defined by the
-extending classes
+extending classes.
 
 =head2 ProblemLink
 
-A url pointing to a problem setup on L<< http://projecteuler.net >>
+A url pointing to a problem definition on L<< http://projecteuler.net >>.
+
+=head3 Definition
 
     as Str,
-    message { "$_ is not a a valid link" },
+    message { sprintf(q{'%s' is not a valid link}, $_ // '#UNDEFINED#') },
     where { $_ =~ m{
                 \A
                 \Qhttp://projecteuler.net/index.php?section=problems&id=\E
@@ -73,14 +70,17 @@ subtype ProblemLink,
     };
 
 
+
 =head2 ProblemName
 
 In an effort to limit text runoff, the problem name is limited to 80
 characters.  Similarly, the length must also be greater than 10 to ensure it is
-a usefull name.
+something usefull.
+
+=head3 Definition
 
     as Str,
-    message { qq{'$_' must be a a string between 10 and 80 characters long} },
+    message { sprintf(q{'%s' must be a string between 10 and 80 characters long}, $_ // '#UNDEFINED#') },
     where {
         length $_ > 10  and  length $_ < 80;
     };
@@ -95,18 +95,22 @@ subtype ProblemName,
     };
 
 
+
 =head2 PosInt
 
-An integer greater than 0
+An integer greater than 0.
+
+=head3 Definition
 
     as Int,
+    message { sprintf(q{'%s' is not greater than 0}, $_ // '#UNDEFINED#') },
     where {
         $_ > 0
     }
 
-=head3 PosIntArray
+=head2 PosIntArray
 
-An array of PosInts
+An array of PosInts.
 
 =cut
 
@@ -119,18 +123,22 @@ subtype PosInt,
 subtype PosIntArray, as ArrayRef[PosInt];
 
 
+
 =head2 NegInt
 
-An integer less than 0
+An integer less than 0.
+
+=head3 Definition
 
     as Int,
+    message { sprintf(q{'%s' is not less than 0}, $_ // '#UNDEFINED#') },
     where {
         $_ < 0
     }
 
-=head3 NegIntArray
+=head2 NegIntArray
 
-An array of NegInts
+An array of NegInts.
 
 =cut
 
@@ -143,9 +151,12 @@ subtype NegInt,
 subtype NegIntArray, as ArrayRef[NegInt];
 
 
+
 =head2 MyDateTime
 
-A L<< DateTime >> object parsed using L<< DateTime::Format::DateParse >>
+A L<< DateTime >> object coerced using L<< DateTime::Format::DateParse >>
+
+=head3 Definition
 
     class_type MyDateTime, { class => 'DateTime' };
     coerce MyDateTime,
@@ -166,35 +177,5 @@ coerce MyDateTime,
         DateTime::Format::DateParse->parse_datetime( $_ );
     };
 
-=head1 AUTHOR
-
-Adam Lesperance, C<< <lespea at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-project-euler at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Project-Euler>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Project::Euler::Lib::Types
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Adam Lesperance.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
 
 1; # End of Project::Euler::Lib::Types
